e01beb8d030a8cc78cce0a7c0ecc9e22e3a7f856
james chandler <stillsound@protonamil.com>
2021-07-28 11:01:09 -0700

    map view renders/ data from model connects

--- routes/map.js
+++ routes/map.js
@@ -2,5 +2,7 @@ const express = require('express');
 const router = express.Router();
 const eventCtrl = require('../controllers/map');
 
-router.get('/', eventCtrl.render);
+// router.get('/', eventCtrl.render);
+router.get('/', eventCtrl.locationList)
+
 module.exports = router;
