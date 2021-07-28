d6641d08f6473ad3ec1ce76759ce91e2f12c6190
james chandler <stillsound@protonamil.com>
2021-07-27 18:58:22 -0700

    socket.io recieve message on console

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const logger = require('morgan');
 const mongoose = require('mongoose');
 const session = require("express-session");
 const passport = require('passport');
+
+
 /* ====== Internal Modules  ====== */
-const routes = require('./routes')
 // all code that is our code 
-
-
+const routes = require('./routes')
 
 /* ====== Instanced Module  ====== */
 // Create the Express App
@@ -25,10 +25,10 @@ app.set("view engine", "ejs");//set view engine to ejs
 require('./config/database'); //bring in the Nemesis database
 require('./config/passport')
 
-
 /*======= Middleware ======*/
 app.use(logger('morgan'));
 app.use(express.static('public'));
+app.use(express.static(__dirname + '/node_modules'));  
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
@@ -46,8 +46,31 @@ app.use(passport.session());
 app.use('/', routes.auth)
 app.use('/user', routes.user)
 
+//route for chat
+app.get('/', function(req, res, next) {  
+    res.sendFile(__dirname + '/index.html');
+});
+
 /* ====== Server bind  ====== */
 // bind the application to the port via app.listen
-app.listen(PORT, function () {
+const server = app.listen(PORT, function () {
     console.log(`Nemesis bound and awaiting orders at http://localhost:${PORT}`);
   });
+
+  /*======= socket.io ============*/
+const socket = require('socket.io');
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log('socket connect');
+  socket.on('disconnect', () => {
+    console.log('socket disconnected');
+  });
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
+});
+
