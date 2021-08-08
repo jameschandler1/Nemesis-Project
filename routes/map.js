f38dfd635017669f3361143865b0f72ee26f96d8
Veritas <rose.daemon.belmont@gmail.com>
2021-08-08 10:11:48 -0700

    removes vestigial code

--- routes/map.js
+++ routes/map.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const eventCtrl = require('../controllers/map');
 
 
-// router.get('/', eventCtrl.render);
+
 router.get('/', eventCtrl.locationList)
 
 module.exports = router;
